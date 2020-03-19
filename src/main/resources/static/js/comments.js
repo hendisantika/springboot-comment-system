18400231c9dffd45aa39263bca6caf2407891569
Hendi Santika <hendisantika@gmail.com>
2020-03-20 06:38:54 +0700

    #18 - Add showComments2 in comments.js file

--- src/main/resources/static/js/comments.js
+++ src/main/resources/static/js/comments.js
@@ -8,3 +8,39 @@ function showComments() {
         }
     })
 }
+
+$(document).ready(function () {
+    showComments();
+    $('#commentForm').on('submit', function (event) {
+        event.preventDefault();
+        var formData = $(this).serializeArray();
+        var formDataObject = {};
+        $.each(formData,
+            function (i, v) {
+                formDataObject[v.name] = v.value;
+            });
+        console.log(formDataObject);
+        $.ajax({
+            //Post comment
+            url: "http://localhost:8080/api/comments",
+            method: "POST",
+            data: formDataObject,
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: false
+            },
+
+            success: function (response) {
+                showComments();
+            }
+        })
+    });
+    $(document).on('click', '.reply', function () {
+        //Reply to this id
+        var commentId = $(this).attr("id");
+        $('#parent_id').val(commentId);
+        $('#name').focus();
+
+    });
+});
+
