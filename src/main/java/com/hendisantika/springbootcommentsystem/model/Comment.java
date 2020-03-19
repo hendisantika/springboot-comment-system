f16453a11f70d1603bbf9e92bf203e5bfeed961b
Hendi Santika <hendisantika@gmail.com>
2020-03-20 06:30:27 +0700

    #5 - Add Comment Model

---
+++ src/main/java/com/hendisantika/springbootcommentsystem/model/Comment.java
@@ -0,0 +1,52 @@
+package com.hendisantika.springbootcommentsystem.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-comment-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/03/20
+ * Time: 06.29
+ */
+@Entity
+@Table(name = "comment")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt"},
+        allowGetters = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String content;
+    private Long parentId;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
